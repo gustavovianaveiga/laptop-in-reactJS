26c13ff63444cc1ba40a13d9cc2c8e8e4e9b995e
gustavo viana <gustavovianaveiga721@gmail.com>
2022-05-25 11:16:07 -0300

    backup da merda q eu tinha feito

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/laptop-in-react/',
-    plugins: [react()]
+  plugins: [react()]
 })
